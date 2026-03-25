bd1172716483c9180646e67cc8eaf9a6232c07a6
anya <nyuta1909@gmail.com>
2026-03-25 13:21:06 +0300

    первое задание

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
+chdir('/var/www/alekseeva.net');
+$file = fopen("test.txt", 'w') or
+die("не удалось создать файл");
+fwrite($file, "Привет, мир!");
+fclose($file);
+
+echo "<h1>Проверь папку сайта :))</h1>";
 

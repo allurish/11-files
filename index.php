532d15851c39cb4148e8d7121dd80f940535f4c8
anya <nyuta1909@gmail.com>
2026-03-25 13:29:25 +0300

    второе задание

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ $file = fopen("test.txt", 'w') or
 die("не удалось создать файл");
 fwrite($file, "Привет, мир!");
 fclose($file);
+echo "<h1>1. Проверь папку сайта :))</h1>";
 
-echo "<h1>Проверь папку сайта :))</h1>";
+$content = htmlentities(file_get_contents("test.txt"));
+echo "<h1>2. Содержимое файла test.txt: $content</h1>";
+
 

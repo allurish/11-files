95dad0dfc00f6d22b2de414b37b3bf4a07db911a
anya <nyuta1909@gmail.com>
2026-03-25 17:13:38 +0300

    четвёртое задание

--- index.php
+++ index.php
@@ -19,4 +19,15 @@ if(rmdir("www"))
     echo "<h1>2. Папка ввв успешно удалена</h1>";
 else echo "<h1>2. Ошибка при удалении папки ввв</h1>";
 
+$folders = ["topSecret", "myFolder", "kittens"];
+foreach ($folders as $f) {
+    if(file_exists($f))
+    echo "<h2>Папка с названием $f уже существует</h1>";
+else {
+    if (mkdir($f))
+        echo "<h2>Папка $f успешно создана</h1>";
+    else echo "<h2>Ошибка при создании папки $f</h1>";
+}
+}
+
 

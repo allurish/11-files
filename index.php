8b8355cd5f82ed57fcd0cb3a3fc2d9e685a7e981
anya <nyuta1909@gmail.com>
2026-03-25 14:01:53 +0300

    четвёртое задание

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <?php
-if (unlink("test.txt"))
-    echo "Файл удален";
-else echo "Ошибка при удалении файла";
-if (unlink("mir.txt"))
-    echo "Файл удален";
-else echo "Ошибка при удалении файла";
+unlink("test.txt");
+unlink("mir.txt");
+unlink("/folder/mir.txt");
+rmdir("/var/www/alekseeva.net/folder");
 
 
 chdir('/var/www/alekseeva.net');
-$file = fopen("test.txt", 'w') or
+$file = fopen("test.txt", 'w+') or
 die("не удалось создать файл");
 fwrite($file, "Привет, мир!");
 fclose($file);
@@ -18,6 +16,16 @@ $content = htmlentities(file_get_contents("test.txt"));
 echo "<h1>2. Содержимое файла test.txt: $content</h1>";
 
 if(!rename("test.txt", "mir.txt"))
-    echo "<h1>3. Ошибка переименован файла</h1>";
+    echo "<h1>3. Ошибка переименования файла</h1>";
 else echo "<h1>3. Файл успешно переименован</h1>";
 
+if(mkdir("folder", 0775))
+    echo "Каталог создан";
+else echo "Ошибка при создании каталога";
+if(!rename("mir.txt", "/var/www/alekseeva.net/folder/mir.txt"))
+    echo "<h1>4. Ошибка перемещения файла</h1>";
+else echo "<h1>4. Файл успешно перемещён в новый каталог</h1>";
+
+if (!copy("/var/www/alekseeva.net/folder/mir.txt", "/var/www/alekseeva.net/folder/world.txt"))
+    echo "<h1>5. Ошибка копирования файла</h1>";
+else echo "<h1>5. Файл скопирован, проверь папку</h1>";

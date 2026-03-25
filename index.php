7006bdd7a88b958c2e068c941d6565b136586dab
anya <nyuta1909@gmail.com>
2026-03-25 13:40:54 +0300

    третье задание

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+if (unlink("test.txt"))
+    echo "Файл удален";
+else echo "Ошибка при удалении файла";
+if (unlink("mir.txt"))
+    echo "Файл удален";
+else echo "Ошибка при удалении файла";
+
+
 chdir('/var/www/alekseeva.net');
 $file = fopen("test.txt", 'w') or
 die("не удалось создать файл");
@@ -9,4 +17,7 @@ echo "<h1>1. Проверь папку сайта :))</h1>";
 $content = htmlentities(file_get_contents("test.txt"));
 echo "<h1>2. Содержимое файла test.txt: $content</h1>";
 
+if(!rename("test.txt", "mir.txt"))
+    echo "<h1>3. Ошибка переименован файла</h1>";
+else echo "<h1>3. Файл успешно переименован</h1>";
 

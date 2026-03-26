e28b996d1b025799bd92f0127e2890217f5ea75e
anya <nyuta1909@gmail.com>
2026-03-26 10:29:52 +0300

    задания 1 и 2 второй части

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
 
-//privet
+$march12 = mktime(10, 25, 0, 3, 15, 2025);
+echo date('1. H:i:s d.m.Y', $march12) . "<br>";
+
+echo '2. ' . time() - mktime(8, 5, 59, 10, 2, 1990) . ' секунд<br>';

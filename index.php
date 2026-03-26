70bf183a4610ecfd4251b4cab3ea21d5474a765a
anya <nyuta1909@gmail.com>
2026-03-26 10:32:56 +0300

    задания 3 и 4 второй части

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
-
+date_default_timezone_set('Europe/Moscow');
 $march12 = mktime(10, 25, 0, 3, 15, 2025);
-echo date('1. H:i:s d.m.Y', $march12) . "<br>";
+echo date('1. H:i:s d.m.Y', $march12) . '<br>';
 
 echo '2. ' . time() - mktime(8, 5, 59, 10, 2, 1990) . ' секунд<br>';
+
+echo date('3. Y.m.d H:i:s') . '<br>';
+
+echo date('4. Y.m.d', mktime(0,0,0,9,1)) . '<br>';

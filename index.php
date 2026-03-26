3e7bd5a23c7885a6ab93571dab92e36326c16d53
anya <nyuta1909@gmail.com>
2026-03-26 10:09:46 +0300

    второе задание первой части

--- index.php
+++ index.php
@@ -11,3 +11,13 @@ try {
 } catch(Exception $ex) {
     echo $ex->getMessage();
     }
+
+try {
+    echo "15 / 5 = " . 15/5 . "<br>";
+    echo "169 / 13 = " . 169/13 . "<br>";
+    echo "32 / 0 = ";
+    echo 32/0 . "<br>";
+    echo "32 / 4 = " . 32/4 . "<br>";
+} catch(DivisionByZeroError $ex) {
+    echo "<h1>2. На ноль делить нельзя</h1>";
+}

f160f724495c8d765f9d1219b296d9c97cfc9e9e
anya <nyuta1909@gmail.com>
2026-03-26 09:39:48 +0300

    первое задание первой части

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
 
+$filename = "nonexistent.txt";
+if(file_exists($filename)) {
+    if(fopen($filename))
+        echo "<h1>1. Файл отркыт</h1>";
+    else echo "<h1>1. Ошибка при отркытии файла</h1>";
+}
+else echo "<h1>1. Такого файла не существует</h1>";

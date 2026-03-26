887c7b42e7d61d88d47fd3541d189fa241cc67ab
anya <nyuta1909@gmail.com>
2026-03-26 10:04:37 +0300

    первое задание первой части (испрвлено)

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 
 $filename = "nonexistent.txt";
-if(file_exists($filename)) {
-    if(fopen($filename))
-        echo "<h1>1. Файл отркыт</h1>";
-    else echo "<h1>1. Ошибка при отркытии файла</h1>";
-}
-else echo "<h1>1. Такого файла не существует</h1>";
+
+try {
+    if(!file_exists($filename))
+        throw new Exception("<h1>1. Ошибка: такого файла не существует</h1>");
+    $file = fopen($filename, 'r');
+    echo "<h1>1. Файл отркыт</h1>";
+    fclose($file);
+} catch(Exception $ex) {
+    echo $ex->getMessage();
+    }

668e6b59f4226de82cf59f5590dd288be9ed66ce
anya <nyuta1909@gmail.com>
2026-04-08 13:27:21 +0300

    getters

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
 <?php
 
 class Worker {
-    public $name, $age, $salary;
+    private $name, $age, $salary;
     function __construct($name, $age, $salary) {
         $this->name = $name;
         $this->age = $age;
         $this->salary = $salary;
     }
+    function getName() { return $this->name; }
+    function getAge() { return $this->age; }
+    function getSalary() { return $this->salary; }
 }
 
 $bob = new Worker("Robert", 34, 4000);
 $amy = new Worker("Amelie", 28, 3200);
-echo "sum of workers' salaries: $" . $bob->salary + $amy->salary;
+echo "sum of workers' (" . $bob->getName() . " and " . $amy->getName() . ") salaries: $" . $bob->getSalary() + $amy->getSalary();
+echo "<br>";
+echo "sum of workers' (" . $bob->getName() . " and " . $amy->getName() . ") ages: " . $bob->getAge() + $amy->getAge();
+
 

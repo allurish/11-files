73198dcad9b9e5a9ba999c991ce48da14c6f343f
anya <nyuta1909@gmail.com>
2026-04-08 13:16:25 +0300

    class worker created

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
-//13 лабораторная
+class Worker {
+    public $name, $age, $salary;
+    function __construct($name, $age, $salary) {
+        $this->name = $name;
+        $this->age = $age;
+        $this->salary = $salary;
+    }
+}
 
+$bob = new Worker("Robert", 34, 4000);
+$amy = new Worker("Amelie", 28, 3200);
+echo "sum of workers' salaries: $" . $bob->salary + $amy->salary;
 

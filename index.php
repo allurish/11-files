5ba2a097daad675361cd1107db46f6efa5f6b33e
anya <nyuta1909@gmail.com>
2026-04-13 15:26:07 +0300

    третье задание

--- index.php
+++ index.php
@@ -5,4 +5,20 @@
 abstract class Figure {
     public $area, $color, $sides;
     abstract public function infoAbout();
-}
+};
+
+class Rectangle extends Figure {
+    public function infoAbout() {
+        echo "rectangle<br>";
+    }
+};
+class Triangle extends Figure {
+    public function infoAbout() {
+        echo "triangle<br>";
+    }
+};
+class Square extends Figure {
+    public function infoAbout() {
+        echo "square<br>";
+    }
+};

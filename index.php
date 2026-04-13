aab4ca4cc0bfc523770bb96606477a4e2c61e9ef
anya <nyuta1909@gmail.com>
2026-04-13 15:31:26 +0300

    задания 4.1-8

--- index.php
+++ index.php
@@ -12,16 +12,34 @@ interface IFigure {
 }
 
 class Rectangle extends Figure implements IFigure {
+    public function __construct(private $a, private $b)
+    {
+        $this->a = $a;
+        $this->b = $b;
+        $sides = 4;
+    }
     public function infoAbout() {
         echo "rectangle<br>";
     }
 };
 class Triangle extends Figure implements IFigure {
+    public function __construct(private $a, private $b, private $c)
+    {
+        $this->a = $a;
+        $this->b = $b;
+        $this->c = $c;
+        $sides = 3;
+    }
     public function infoAbout() {
         echo "triangle<br>";
     }
 };
 class Square extends Figure implements IFigure {
+    public function __construct(private $a)
+    {
+        $this->a = $a;
+        $sides = 4;
+    }
     public function infoAbout() {
         echo "square<br>";
     }
